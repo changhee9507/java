549537570f85b6c55878fcec9d36503ffdb3e7a7
SP20141391 <ckdgml1981@unist.ac.kr>
2020-01-23 22:29:03 +0900

    Initializtion

---
+++ variable/Initialization.java
@@ -0,0 +1,23 @@
+package variable;
+
+public class Initialization {
+	public static void main(String[] args) {
+		/* 변수를 선언하고 초기화하는 다양한 방법 */
+		int a;
+		int b,c;
+		int d = 3;
+		int e = 4, f =5;
+		
+		a = 0;
+		b = 1;
+		c = 2;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+				
+	}
+}
