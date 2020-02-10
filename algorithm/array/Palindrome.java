39aae88e04aeb6de85cb1a1223a296352149840a
SP20141391 <ckdgml1981@unist.ac.kr>
2020-02-11 00:25:17 +0900

    update

---
+++ algorithm/array/Palindrome.java
@@ -0,0 +1,35 @@
+package array;
+
+import java.util.Scanner;
+
+public class Palindrome {
+	
+	static char[][] input = new char[8][8];
+	static int TESTCASE = 10;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		for(int testCase = 0; testCase < TESTCASE; testCase++) {
+			int palLength = scanner.nextInt();
+			
+			/* save input to array */
+			
+			for(int i=0; i<8; i++) {
+				for(int j=0; j<8; j++) {
+					
+					
+					
+					
+				}
+			}
+			
+		}
+		
+		
+		
+	}
+
+}
--- algorithm/etc/Refrigerator.java
+++ algorithm/etc/Refrigerator.java
@@ -1,7 +1,7 @@
 /**
- * 정올 1828 냉장고 문제 풀이
+ * JungAl 1828 solution
  * 
- * 
+ * @author Changhee Yoon
  */
 
 package etc;
@@ -10,12 +10,24 @@ import java.util.Scanner;
 
 public class Refrigerator {
 
+	static int refNum = 0;
+	static int[][] chemicalInfo;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
 		Scanner scanner = new Scanner(System.in);
 		final int chemicalNum = scanner.nextInt();
+		
+		chemicalInfo = new int[chemicalNum][3];
+		
+		/* 화학약품 온도 정보 저장*/
+		for(int i =0; i<chemicalNum; i++) {
+			chemicalInfo[i][0] = scanner.nextInt(); // 시작점
+			chemicalInfo[i][1] = scanner.nextInt(); // 끝점
+		}
+		
+				
 		
 		
 		
