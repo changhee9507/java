609c26a35fa7015036592e62d98419e41fd5b1eb
윤창희 <changhee9507@gmail.com>
2020-02-12 13:59:13 +0900

    update

--- algorithm/Combination/Gerrymandering.java
+++ algorithm/Combination/Gerrymandering.java
@@ -9,140 +9,100 @@ package Combination;
 
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Gerrymandering {
 
-	static boolean[][] adjMatrix;
-	static int[] population;
-	static boolean[] isInComb;
-	static int size;
-	static int minGap = 99999;
+	/* 스태틱은 위에서 정의해두고 사용전에 초기화 되도록 사용하는 것이 좋을 것 같다 */
+	
+	static int regionNum;
+	static Stack<boolean[]> combinations = new Stack<boolean[]>();
+	static boolean[] used;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		Scanner scan = new Scanner(System.in);
-		size = scan.nextInt();
-
-		adjMatrix = new boolean[size + 1][size + 1];
-		population = new int[size + 1];
-		isInComb = new boolean[size + 1];
-
-		/* 인구 정보 저장 */
-		for (int i = 1; i <= size; i++) {
-			population[i] = scan.nextInt();
+		regionNum = scan.nextInt();
+		Region[] regions = new Region[regionNum];
+		used = new boolean[regionNum];
+		
+		/* 지역마다 인구 저장 */
+		for (int i = 0; i < regionNum; i++) {
+			int population = scan.nextInt();
+			regions[i] = new Region(population);
 		}
 
-		/* 인접 정보 저장 */
-		for (int i = 1; i <= size; i++) {
-			int num = scan.nextInt();
-			for (int j = 1; j <= num; j++) {
-				int region = scan.nextInt();
-				adjMatrix[i][region] = true;
+		/* 연결정보 저장 */
+		for (int i = 0; i < regionNum; i++) {
+			int connectionNum = scan.nextInt();
+
+			for (int j = 0; j < connectionNum; j++) {
+				regions[i].connectedRegion.push(scan.nextInt());
 			}
 		}
 
-		/* 연결 되어 있으면 모두 추가 */
+		/* 입력이 잘 되었는지 확인 */
+		for (int i = 0; i < regionNum; i++) {
+			System.out.println("region " + i + " " + regions[i].population);
+			while (!regions[i].connectedRegion.isEmpty()) {
+				System.out.println(regions[i].connectedRegion.pop());
+			}
+		}
 
-		/* 인접 정보 출력 */
-		/*
-		 * for (int i = 0; i < size + 1; i++) { for (int j = 0; j < size + 1; j++) {
-		 * System.out.print(adjMatrix[i][j] + " "); } System.out.println(); }
-		 */
+		/* 조합 생성 */
+		for (int i = 1; i <= (regionNum/2); i++) {
+			genComb(0, i);
+		}
+		
+		/* 조합 확인 */
+		/*while(!combinations.isEmpty()) {
+			System.out.println(Arrays.toString(combinations.pop()));
+		}*/
 
-		System.out.println(Arrays.toString(population));
+		/* 연결 만들기 */
+		/* 불린 배열하나 만들어서 */
+		while(!combinations.isEmpty()) {
+			boolean[] check = combinations.pop();
+			
+		}
+		
+		
+		
+		/* 인구 차이 계산 */
 
-		
-		for(int i =1; i<=size/2 ; i++) { combination(1,0,i); }
-		 
-		
-		
-		System.out.println(minGap);
+		/* 최소값 갱신 */
+
+	}
+	
+	/* 함수를 사용할 때 처럼 메인에서 클래스를 사용하려면 static이 필수적이다 */
+	static public class Region {
+
+		int population = 0;
+		Stack<Integer> connectedRegion = new Stack<Integer>();
+
+		public Region(int population) {
+			this.population = population;
+
+		}
 	}
 
-	static void connection() {
-		for (int i = 1; i <= size; i++) {
-			for (int j = 1; j <= size; j++) {
-				if (adjMatrix[i][j] == true) {
-					if (isInComb[i] == isInComb[j]) {
+	
 
-						for (int k = 1; k <= size; k++) {
-							if (adjMatrix[j][k] == true) {
-								adjMatrix[i][k] = true;
-							}
-						}
-
-					}
-
-				}
-			}
-		}
-	}
-
-	static void combination(int start, int count, int depth) {
-		/* 카운트는 뽑는 숫자의 갯수가 된다. 카운트가 1이라면 원소가 한개인 것들 */
-		if (count == depth) {
-			System.out.println(Arrays.toString(isInComb));
-
-			/* 이 조합에서 연결 상태를 갱신 */
-			connection();
-
-			/* 연결 되어 있지 않으면 return */
-			for (int i = 1; i <= size; i++) {
-				for (int j = 1; j <= size; j++) {
-					if (i == j)
-						continue;
-					else {
-						if (isInComb[i] == isInComb[j]) {
-							if (adjMatrix[i][j] == false) {
-								return;
-							}
-						}
-					}
-				}
-			}
+	static public void genComb(int count, int selectionNum) {
+		if (count == selectionNum) {
 			
-			
-			
-			int trueSum = 0;
-			int falseSum = 0;
-			
-			for(int i = 1; i<size; i++) {
-				if(isInComb[i] == true) {
-					trueSum += population[i];
-				}else {
-					falseSum += population[i];
-				}
-			}
-			
-			System.out.println("trueSum " + trueSum + " falseSum " + falseSum);
-			int gap = (trueSum - falseSum);
-			
-			if(gap>= 0 && gap < minGap) {
-				minGap = gap;
-			}
-			
-			if(gap <0 && (-1*gap) < minGap ) {
-				minGap = -1 * gap;
-			}
-			
-
-			/* 인접 정보 출력 */
-			for (int i = 0; i < size + 1; i++) {
-				for (int j = 0; j < size + 1; j++) {
-					System.out.printf("%6s ", adjMatrix[i][j]);
-				}
-				System.out.println();
-			}
-
-			return;
+			/*System.out.println("push" + Arrays.toString(used));*/
+			combinations.push(Arrays.copyOf(used, used.length));
 		}
 
-		for (int i = start; i <= size; i++) {
-			if (isInComb[i] == true)
+		for (int i = 0+count; i < regionNum; i++) {
+			if (used[i] == true) {
 				continue;
-			isInComb[i] = true;
-			combination(start + 1, count + 1, depth);
-			isInComb[i] = false;
+			}
+			
+			used[i] = true;
+			genComb(count + 1, selectionNum);
+			used[i] = false;
 		}
 	}
 }
---
+++ algorithm/sort/CompareableComparator.java
@@ -0,0 +1,119 @@
+/**
+ * Comporator이 우선 순위가 높다.
+ * 
+ * Comparable의 compareTo는 어떤 객체 자체가 비교를 가능하도록 만들고
+ * Comporator은 상황에 맞춰서 비교를 한다.
+ * 
+ */
+package sort;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class CompareableComparator {
+
+		   static class Student implements Comparable<Student>{
+		      int no,score;
+
+		      public Student(int no, int score) {
+		         super();
+		         this.no = no;
+		         this.score = score;
+		      }
+
+		      @Override
+		      public String toString() {
+		         return "Student [no=" + no + ", score=" + score + "]";
+		      }
+		      @Override
+		      public int compareTo(Student o) {
+		         return this.no - o.no;
+		      }
+		      
+		   }
+	
+		   
+		   public static void main(String[] args) {
+		      int[] arr = new int[] {4,3,7,9,10};
+		      System.out.println("초기 배열 출력");
+		      System.out.println(Arrays.toString(arr));
+		      Arrays.sort(arr);
+		      
+		      System.out.println("Arrays.sort 이후 출력");
+		      System.out.println(Arrays.toString(arr));
+		      
+		      
+		      Integer[] arr2 = new Integer[] {new Integer(4),3,7,9,10};
+		      System.out.println("Integer 타입 원소 배열 출력");
+		      System.out.println(Arrays.toString(arr2));
+		      /* Arrays.sort(arr2);*/ // 사용가능 
+		     
+		      
+		      /* 원소의 타이비 comparator에 들어간다. */
+		      Arrays.sort(arr2, new Comparator<Integer>() {
+		         @Override
+		         public int compare(Integer o1, Integer o2) {
+		           /* return o2.intValue()-o1.intValue();*/ // 양수와 음수가 섞일때 문제가 생긴다.
+		            return o2.compareTo(o1);
+		         }
+		      } );
+		      System.out.println(Arrays.toString(arr2));
+		      
+		      
+		      
+		      System.out.println(" 2차 배열의 정렬, 첫 원소 기준 ");
+		      int[][] arr3 = new int[][]{{1,10},{3,50}, {8,50},{2,80},{4,10}};
+		      
+		      Arrays.sort(arr3, new Comparator<int[]>() {
+
+		         @Override
+		         public int compare(int[] o1, int[] o2) {
+		            return o1[0] - o2[0];
+		         }
+		      });       
+		      
+		      
+		      for (int[] ar : arr3) {
+		         System.out.println(Arrays.toString(ar));
+		      }
+		      
+		      
+		      
+		      System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>.2.");
+		      Arrays.sort(arr3, new Comparator<int[]>() {
+		         
+		         @Override
+		         public int compare(int[] o1, int[] o2) {
+		            int result = o1[1] - o2[1];
+		            if(result==0) {
+		               result = o1[0] - o2[0];
+		            }
+		            return result;
+		         }
+		      });
+		      for (int[] ar : arr3) {
+		         System.out.println(Arrays.toString(ar));
+		      }
+		      
+		      Student[] arr4 = new Student[] {
+		         new Student(1, 10),   
+		         new Student(3, 50),   
+		         new Student(2, 80),   
+		         new Student(4, 10)   
+		      };
+		      
+		      System.out.println("=========================");
+//		      Arrays.sort(arr4);
+		      Arrays.sort(arr4, new Comparator<Student>() {
+
+		         @Override
+		         public int compare(Student o1, Student o2) {
+		            return o2.score - o1.score;
+		         }
+		      });
+		      for (Student student : arr4) {
+		         System.out.println(student);
+		      }
+		   }
+
+}
