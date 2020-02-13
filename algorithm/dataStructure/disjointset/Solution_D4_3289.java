f012790803bfaaf3f7c898ba98f4555d393cb20f
SP20141391 <ckdgml1981@unist.ac.kr>
2020-02-14 01:08:59 +0900

    update

--- algorithm/dataStructure/disjointset/Solution_D4_3289.java
+++ algorithm/dataStructure/disjointset/Solution_D4_3289.java
@@ -4,70 +4,57 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution_D4_3289 {
-	
+
 	static int[] parents;
-	static StringBuilder result = new StringBuilder();
-	
+	static StringBuilder sb;
+	static int eleNum;
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		Scanner scan = new Scanner(System.in);
-		
-		final int TESTNUM = scan.nextInt();
-		
-		for(int testCase = 1; testCase<= TESTNUM; testCase++) {
-			parents = new int[scan.nextInt()+1];
+		final int TESTCASE = scan.nextInt();
+
+		for (int testCase = 1; testCase <= TESTCASE; testCase++) {
+			eleNum = scan.nextInt();
+			parents = new int[eleNum + 1];
 			Arrays.fill(parents, -1);
-			
 			int opNum = scan.nextInt();
-			
-			
-			for(int j = 1; j <= opNum; j++) {
+			sb = new StringBuilder();
+
+			for (int j = 1; j <= opNum; j++) {
 				int op = scan.nextInt();
-				int firstElement = scan.nextInt();
-				int secondElement = scan.nextInt();
-				
-				if(op == 0) {
-					union(firstElement, secondElement);
+				int ele1 = scan.nextInt();
+				int ele2 = scan.nextInt();
+
+				int rep1 = getRepresentor(ele1);
+				int rep2 = getRepresentor(ele2);
+
+				if (op == 0) { /* union */
+					if(rep1 == rep2) continue;
+					else {
+						parents[ele2] = rep1;
+					}
+				} else { /* check whether they are in same set */
+
+					if (rep1 == rep2) {
+						sb.append(1);
+					} else {
+						sb.append(0);
+					}
 				}
-				
-				if(op == 1) {
-					check(firstElement, secondElement);
-				}
-				
+
+				System.out.println(Arrays.toString(parents));
 			}
-			
-			System.out.printf("#%d %s\n",testCase, result.toString());
+			System.out.printf("#%d %s\n", testCase, sb.toString());
 		}
-		
-	}
-	
-	private static void check(int ele1, int ele2) {
-		int parent1=0;
-		int parent2=0;
-		
-		while(parents[ele1] < 0) {
-			ele1 = parents[ele1];
-			if(ele1 <0 ) {
-				break;
-			}
-		}
-		
-		while(parents[ele2] < 0) {
-			ele2 = parents[ele2];
-			if(ele2 <0 ) {
-				break;
-			}
-		}
-		
-		if(ele1 == ele2) {
-			result.append(1);
-		}else {
-			result.append(0);
-		}
-	}
-	
-	private static void union(int ele1, int ele2) {
-		parents[ele2] = ele1;
+
 	}
 
+	static int getRepresentor(int ele) {
+		if (parents[ele] < 0)
+			return ele;
+		else {
+			return parents[ele] = getRepresentor(parents[ele]);
+		}
+	}
 }
